0c10e0b94219843aab1ef08b971bcd31fcac2374
JCBackendDev <jcbackenddev@gmail.com>
2021-12-16 15:17:36 +0100

    added image links object

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -15,31 +15,31 @@ export class SlidesComponent implements OnInit {
   images = [
     {
       title: 'tree',
-      url: ''
+      url: 'https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'lake',
+      url: 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdhbGxwYXBlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'mountain',
+      url: 'https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'city',
+      url: 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'insdustry',
+      url: 'https://images.unsplash.com/photo-1516937941344-00b4e0337589?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aW5kdXN0cnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'hamburg',
+      url: 'https://images.unsplash.com/photo-1625425311066-c4525908ef8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGRvY2tzJTIwaGFtYnVyZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
     },
     {
-      title: 'tree',
-      url: ''
+      title: 'fear',
+      url: 'https://images.unsplash.com/photo-1413847394921-b259543f4872?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
     },
   ]
 
