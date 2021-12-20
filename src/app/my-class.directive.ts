35616d219edc8de9bca470ff73034f86164a95ec
JCBackendDev <jcbackenddev@gmail.com>
2021-12-20 13:34:25 +0100

    adding Input parameter on custom Directive
    It is nt working though, because Angular is way too fast!!!! nöööööö!

--- src/app/my-class.directive.ts
+++ src/app/my-class.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 // This is a custom directive adding a organge background to the html it is called upon
 @Directive({
   selector: '[appMyClass]'
 })
 export class MyClassDirective {
+  @Input() backgroundColor!: string;
 
   // ElementRef allows us to automatically get the HTML-element the directive was called upon to as a parameter.
   constructor(private element: ElementRef) { 
-
+    
     // element = our parameter
     // nativeElement = the actual HTML element
-    this.element.nativeElement.style.backgroundColor = 'orange';
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
   }
 
 }
