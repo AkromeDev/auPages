6edd7550b94ba25007e2c65807a734ad160cbd3a
JCBackendDev <jcbackenddev@gmail.com>
2021-12-20 13:18:34 +0100

    Added custom Directive and used it
    I could have used a css class to add this styling but this is for theheck of learning!!

--- src/app/my-class.directive.ts
+++ src/app/my-class.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
+// This is a custom directive adding a organge background to the html it is called upon
 @Directive({
   selector: '[appMyClass]'
 })
 export class MyClassDirective {
 
-  constructor() { }
+  // ElementRef allows us to automatically get the HTML-element the directive was called upon to as a parameter.
+  constructor(private element: ElementRef) { 
+
+    // element = our parameter
+    // nativeElement = the actual HTML element
+    this.element.nativeElement.style.backgroundColor = 'orange';
+  }
 
 }
