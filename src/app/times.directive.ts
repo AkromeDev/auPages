c44bc3cacda96c470e4723a44471c211509715d7
JCBackendDev <jcbackenddev@gmail.com>
2021-12-21 10:07:33 +0100

    Added logic to appTimes
    Our Directive is now duplication any content inside the element we apply it to, as many times as we indicate it, in its parameter

--- src/app/times.directive.ts
+++ src/app/times.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTimes]'
 })
 export class TimesDirective {
 
-  constructor() { }
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+  ) {}
+    @Input('appTimes') set render(times: number) {
+      this.viewContainer.clear();
 
+      for (let i= 0; i < times; i++) {
+        // This take a look at the viewContainer and create some new HTML inside
+        // The templateRef is the HTML that is inside of the element we applied our directive to
+        this.viewContainer.createEmbeddedView(this.templateRef, {});
+      }
+    }
 }
